fafdde75bb0e15925ce55b2c7d34da72f70cc928
Aswanthcreator1 <fcssethbbbv@gmail.com>
2025-11-15 19:36:04 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -410,14 +410,14 @@ async def cb_handler(client: Client, query: CallbackQuery):
         
     elif query.data == "start":
         buttons = [[
-            [InlineKeyboardButton('⤬ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')],
-            [InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help'),
-             InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about')],
-            [InlineKeyboardButton("ɪɴʟɪɴᴇ", switch_inline_query_current_chat="")],
-            [InlineKeyboardButton('© Dᴍᴄᴀ', callback_data='dmca')]
-        ],[
-            InlineKeyboardButton('© Dᴍᴄᴀ', callback_data='dmca')
-        ]]
+                    InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+                ],[
+                    InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help'),
+                    InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about')
+                ],[
+                    InlineKeyboardButton('ɪɴʟɪɴᴇ', switch_inline_query_current_chat=""),
+                    InlineKeyboardButton('© Dᴍᴄᴀ', callback_data='dmca')
+                 ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
             text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
