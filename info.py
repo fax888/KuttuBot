3cb50fcac22d4ea05da61339b15feb3826e2f832
arushkaru071-byte <arushkaru071@gmail.com>
2025-11-27 17:55:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -47,7 +47,7 @@ DEFAULT_AUTH_CHANNELS = [int(x) for x in environ.get("AUTH_CHANNEL", "-100329317
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://KING_OF:KOTHA@cluster0.i2t5qiv.mongodb.net/?appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://FAX:KOTHA@cluster0.cbphujj.mongodb.net/?appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'files')
 
